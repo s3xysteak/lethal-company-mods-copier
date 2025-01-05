884215ed0bef55a5b1ca27735c535f0da9f497a8
s3xysteak <s3xysteak@outlook.com>
2025-01-05 23:35:17 +0800

    ci: bump options files

--- bump.config.ts
+++ bump.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'bumpp'
 import tauri from 'tauri-version'
 
 export default defineConfig({
+  files: [
+    /** default */
+    'package.json',
+    'package-lock.json',
+    'jsr.json',
+    'jsr.jsonc',
+    'deno.json',
+    'deno.jsonc',
+    /** for tauri */
+    'src-tauri/tauri.conf.json',
+    'src-tauri/Cargo.toml',
+    'src-tauri/Cargo.lock',
+  ],
   execute: tauri(),
 })
